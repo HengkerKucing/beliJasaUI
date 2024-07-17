503465d563fe87f2f36cfa18659eb76fadce218d
HengkerKucing <140718591+HengkerKucing@users.noreply.github.com>
2024-07-17 20:51:02 +0700

    hero session dan pop akun dan implementasi router

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
-// import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import "./Navbar.scss"
 
 const Navbar = () => {
 
     const [active, setActive] = useState(false)
+    const [open, setOpen] = useState(false)
+
+    const {pathname} = useLocation()
 
     const isActive = ()=> {
         window.scrollY > 0 ? setActive(true) : setActive(false)
@@ -17,32 +20,60 @@ const Navbar = () => {
             window.removeEventListener("scroll", isActive)
         }
     }, [])
+
+    const currentUser = {
+        id:1,
+        username:"Aldibek",
+        isSeller:true
+    }
   
     return (
-    <div className={active ? "navbar active" : "navbar"}>
+    <div className={active || pathname !=="/" ? "navbar active" : "navbar"}>
         <div className="container">
             <div className="logo">
-                {/* <Link to="/"> */}
+                <Link to="/" className='link'>
                 <span className='text'>belijasa</span>
-                {/* </Link> */}
+                </Link>
                 <span className='dot'>.com</span>
             </div>
             <div className="links">
-                <span>BeliJasa Business</span>
-                <span>Explore</span>
+                <span>BeliJasa Bisnis</span>
+                <span>Temukan</span>
                 <span>English</span>
-                <span>Sign in</span>
-                <span>Become a Seller</span>
-                <button>Join</button>
+                <span>Masuk</span>
+                {!currentUser?.isSeller && <span>Menjadi Penjual</span>}
+                {!currentUser && <button>Bergabung</button>}
+                {currentUser && (
+                    <div className="user" onClick={()=>setOpen(!open)}>
+                        <img src="https://i.pinimg.com/564x/df/f2/e5/dff2e51ba3eafe2f231e04c4134f7656.jpg" alt="" />
+                        <span>{currentUser?.username}</span>
+                        {open && <div className="options">
+                            {
+                                currentUser?.isSeller && (
+                                    <>
+                                    <Link className='link' to="/mygigs">Jasa</Link>
+                                    <Link className='link' to="/add">Tambah Jasa </Link>
+                                    </>
+                                )}
+                                <Link className='link' to="/orders">Pesanan</Link>
+                                <Link className='link' to="/messages">Pesan</Link>
+                                <Link>Keluar</Link>
+                        </div>}
+                    </div>
+                )}
             </div>    
         </div>
 
-        {active && (
+        {(active || pathname !=="/" ) && (
         <>
             <hr />
             <div className="menu">
-            <span>Test1</span>
-            <span>Test2</span>
+            <Link className='link' to="/">Elektronik & Kelistrikan</Link>
+            <Link className='link' to="/">Kepelatihan & Mengajar</Link>
+            <Link className='link' to="/">Pijat & Pegal</Link>
+            <Link className='link' to="/">Pembuatan Barang</Link>
+            <Link className='link' to="/">Bersih-bersih</Link>
+            <Link className='link' to="/">Pekerjaan Berat & Kasar</Link>
         </div>
         </>
         )}
