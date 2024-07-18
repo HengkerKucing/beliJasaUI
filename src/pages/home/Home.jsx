7adefe479117a13123eca449c5f9ad4dfb634304
HengkerKucing <140718591+HengkerKucing@users.noreply.github.com>
2024-07-18 13:48:16 +0700

    nambah komponen projek card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,9 @@ import "./Home.scss"
 import Featured from '../../components/featured/Featured'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
 import Slide from '../../components/slide/Slide'
-import {cards} from "../../data"
+import {cards, projects} from "../../data"
 import CatCard from '../../components/catCard/CatCard'
+import ProjectCard from '../../components/projectCard/ProjectCard'
 
 const Home = () => {
   return (
@@ -79,6 +80,11 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Slide slidesToShow={3} arrowsScroll={3}>
+        {projects.map(card=>(
+          <ProjectCard key={card.id} item={card}/>
+        ))}
+      </Slide>
     </div>
   )
 }
